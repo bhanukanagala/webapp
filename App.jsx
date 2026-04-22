67bf071ff79f0e8758bd662177e6d44c4fb556f7
Bhanu <89784446+bhanukanagala@users.noreply.github.com>
2026-04-22 03:40:30 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginPage from './LoginPage';  // make sure the filename matches yours
+
+function App() {
+    return (
+        <div>
+            <LoginPage />
+        </div>
+    );
+}
+
+export default App;
